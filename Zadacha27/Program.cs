a6b176885e8353888aec4b64776a4fbbd50837dc
masyaka.94 <masyaka.94@gmail.com>
2022-10-15 13:37:23 +0500

    добавлена задача 27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число А");
+int A = int.Parse(Console.ReadLine()!);
+
+int result = 0;
+
+while (A%10!=0 || A/10!=0)
+{
+    result += A%10;
+    A = A/10;
+}
+Console.WriteLine();
+Console.WriteLine(result);
