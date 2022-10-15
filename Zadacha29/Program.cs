da996bf70811d36aaf4d3c9f0602f72cd069a13e
masyaka.94 <masyaka.94@gmail.com>
2022-10-15 13:55:46 +0500

    добавлена задача 29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+Random Znach = new Random();
+int index = 0;
+
+while (index<array.Length)
+{
+    array[index]=Znach.Next(1,101);
+    Console.Write($"{array[index]} ");
+    index++;
+}
