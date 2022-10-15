1404f661fa90967942e59e465c447295280ae36c
masyaka.94 <masyaka.94@gmail.com>
2022-10-15 13:15:19 +0500

    first commit

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.*/
+
+Console.WriteLine("Введите число А");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число B");
+int B = int.Parse(Console.ReadLine()!);
+
+int result = 1;
+
+for (int i = 1; i<=B; i++)
+{
+result *= A;
+}
+
+Console.WriteLine();
+Console.WriteLine($"{result} - число {A} в степени {B}");
